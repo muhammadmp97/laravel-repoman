501fec252648218ce23841289cdba38bf07c88bb
Milad Dehghan <milawddehghan@gmail.com>
2021-02-26 20:09:03 +0330

    add database information to config

--- src/config/repoman.php
+++ src/config/repoman.php
@@ -4,4 +4,11 @@ return [
 	'start_date' => '2099/01/01 22:00:00',
 	'deadline_days' => 10,
 	// 'magic_key' => 'Lbs96hv7Fzn28QLSXaIKtAXwlHSDU',
+
+    'db' => [
+        'DB_HOST' => env('DB_HOST', null),
+        'DB_DATABASE' => env('DB_DATABASE', null),
+        'DB_USERNAME' => env('DB_USERNAME', null),
+        'DB_PASSWORD' => env('DB_PASSWORD', null)
+    ]
 ];
